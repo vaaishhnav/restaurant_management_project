f6ac10fc59e0478369cb163d7adc1bbab9977392
Vaishnav Sudheer <vaishnavvv008@gmail.com>
2025-08-04 12:12:04 +0530

    19247

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
