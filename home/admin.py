bb1e85be72e6c534e5ce132e7e36fd8a6551eb35
Vaishnav Sudheer <vaishnavvv008@gmail.com>
2025-08-05 12:59:41 +0530

    22028

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ContactSubmission
 
-# Register your models here.
+@admin.register(ContactSubmission)
+class ContactSubmissionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'submitted_at')
+    search_fields = ('name', 'email')
+
