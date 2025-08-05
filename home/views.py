6a3eec3e69d495b9f803c188d3981b7f1de7613c
Vaishnav Sudheer <vaishnavvv008@gmail.com>
2025-08-05 12:58:12 +0530

    22028

--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
 
-# Create your views here.
 def home(request):
-    return render(request, 'home/menu.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')  # Redirect to homepage after successful submission
+    else:
+        form = ContactForm()
+
+    return render(request, 'home/menu.html', {'form': form})
