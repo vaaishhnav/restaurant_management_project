b3578b99ba06db60170068e6fd925dd4618b9404
Vaishnav Sudheer <vaishnavvv008@gmail.com>
2025-08-05 12:51:50 +0530

    19251

--- products/views.py
+++ products/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Item
 from .serializers import ItemSerializer
 
@@ -24,3 +25,25 @@ class ItemView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def menu_list(request):
+    menu = [
+            {
+                "name": "Margherita Pizza",
+                "description": "Classic delight with 100% real mozzarella cheese",
+                "price": 299.00
+            },
+             {
+                "name": "Pasta Alfredo",
+                 "description": "Creamy white sauce pasta with herbs",
+                "price": 249.00
+            },
+            {
+                "name": "Veggie Burger",
+                "description": "Grilled patty with lettuce, tomato, and cheese",
+                "price": 199.00
+            }
+        ]
+        return Response(menu)
