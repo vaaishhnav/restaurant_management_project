f488a6c1aaea135f740e25dd0f0c39a50288b8f3
Vaishnav Sudheer <vaishnavvv008@gmail.com>
2025-08-06 10:37:32 +0530

    22032

--- home/views.py
+++ home/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render, redirect
 from .forms import ContactForm
 
 def home(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/')  # Redirect to homepage after successful submission
-    else:
-        form = ContactForm()
+    restaurant_name = getattr(settings, 'RESTAURANT_NAME', 'My Restaurant')
+    return render(request, 'home/menu.html', {
+            'restaurant_name': restaurant_name
+           })
 
-    return render(request, 'home/menu.html', {'form': form})
+
+
+
+
+
+
+
