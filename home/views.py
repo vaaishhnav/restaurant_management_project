7b6e474d2088fd05f222c837065f932ad8a31f5b
Vaishnav Sudheer <vaishnavvv008@gmail.com>
2025-08-07 12:29:54 +0530

    23305

--- home/views.py
+++ home/views.py
@@ -13,6 +13,15 @@ from django.http import Http404
 def trigger_404(request):
     raise Http404("Page not found")
 
+from django.shortcuts import render
+from .models import Restaurant
+
+def about(request):
+    restaurant = Restaurant.objects.first()
+        return render(request, 'home/about.html', {
+            'restaurant_name': restaurant.name if restaurant else "Our Restaurant"
+    })
+
 
 
 
