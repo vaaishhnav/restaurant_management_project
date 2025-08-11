087659f58f3762b19e235540e2292a99ad2970f7
Vaishnav Sudheer <vaishnavvv008@gmail.com>
2025-08-11 11:09:55 +0530

    25947

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('menu/', views.menu_list, name='menu_list'),
     path('contact/', views.contact, name='contact'),
+    path('', views.homepage, name='homepage'),
 ]
