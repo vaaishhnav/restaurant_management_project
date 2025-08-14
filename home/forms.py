1550445dd837eb8f985d0298d912282991c6af1d
Vaishnav Sudheer <vaishnavvv008@gmail.com>
2025-08-14 12:34:05 +0530

    30893

--- home/forms.py
+++ home/forms.py
@@ -1,7 +1,20 @@
 from django import forms
-from .models import ContactSubmission
+from .models import ContactSubmission, Feedabck
+
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactSubmission
             fields = ['name', 'email']
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        fields = ["comment"]
+        widgets = {
+            "comment": forms.Textarea(attrs={
+                "rows": 5,
+                "placeholder": "Tell us what you think..."
+           })
+        }
+        labels = {"comment": "Your feedback"}
