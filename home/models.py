cf24a184f6f5a7de14e7d9555bc5dfa5665a6c35
Vaishnav Sudheer <vaishnavvv008@gmail.com>
2025-08-19 16:35:40 +0530

    35284

--- home/models.py
+++ home/models.py
@@ -22,3 +22,12 @@ class Feedback(models.Model):
 
     def __str__(self):
         return f"Feedback #{self.id} - {self.submitted_at:%Y-%m-%d %H:%M}"
+
+class RestaurantLocation(models.Model):
+        address = models.CharField(max_length=255)
+            city = models.CharField(max_length=100)
+                state = models.CharField(max_length=100)
+                    zip_code = models.CharField(max_length=20)
+
+                        def __str__(self):
+                                return f"{self.address}, {self.city}, {self.state} - {self.zip_code}"
