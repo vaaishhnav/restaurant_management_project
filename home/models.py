a70e61b73116eda684e11c3b90b0aca30423e90a
Vaishnav Sudheer <vaishnavvv008@gmail.com>
2025-08-20 22:33:57 +0530

    35283

--- home/models.py
+++ home/models.py
@@ -31,3 +31,11 @@ class RestaurantLocation(models.Model):
 
                         def __str__(self):
                                 return f"{self.address}, {self.city}, {self.state} - {self.zip_code}"
+
+class Contact(models.Model):
+        name = models.CharField(max_length=100)
+            email = models.EmailField()
+                submitted_at = models.DateTimeField(auto_now_add=True)
+
+                    def __str__(self):
+                            return f"{self.name} - {self.email}"
