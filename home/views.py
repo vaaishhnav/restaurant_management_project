1280b8668e2e3610b17edc66055108fb5dcb502d
Vaishnav Sudheer <vaishnavvv008@gmail.com>
2025-08-22 12:49:33 +0530

    36963

--- home/views.py
+++ home/views.py
@@ -26,14 +26,16 @@ def contact_us(request):
     return render(request, 'home/contact_us.html')
 
 def homepage(request):
-    try:
-        restaurant = Restaurant.objects.first()
-        restaurant_name = restaurant.name if restaurant else "Our Restaurant"
-    except Exception as e:
-        restaurant_name = "Our Restaurant"
+    query = request.GET.get('q')  # get search term from ?q=
+        if query:
+                menu_items = MenuItem.objects.filter(name__icontains=query)
+                    else:
+                            menu_items = MenuItem.objects.all()  # show all if no query
 
-    return render(request, 'home/homepage.html', {'restaurant_name': restaurant_name })
-
+                                return render(request, 'home/homepage.html', {
+                                        'menu_items': menu_items,
+                                                'query': query,
+                                                    })
 
             
           
