d0ae316e639319b1cebe742a591abb52c2ca657d
Vaishnav Sudheer <vaishnavvv008@gmail.com>
2025-08-22 16:06:49 +0530

    36965

--- products/models.py
+++ products/models.py
@@ -9,12 +9,22 @@ class Item(models.Model):
     def __str__(self):
         return str(self.item_name)
 
+
+
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=7, decimal_places=2)
-    image = models.ImageField(upload_to='menu_images/', blank=True, null=True) 
-    created_at = models.DateTimeField(auto_now_add=True)
+        description = models.TextField(blank=True)
+            price = models.DecimalField(max_digits=7, decimal_places=2)
+                image = models.ImageField(upload_to='menu_images/', blank=True, null=True)  # <-- NEW
+                    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.name
+                        def __str__(self):
+                                return self.name
+
+                                    def get_image_url(self):
+                                            """
+                                                    Helper: returns URL if image exists, else empty string (useful in templates).
+                                                            """
+                                                                    if self.image and hasattr(self.image, 'url'):
+                                                                                return self.image.url
+                                                                                        return ''
